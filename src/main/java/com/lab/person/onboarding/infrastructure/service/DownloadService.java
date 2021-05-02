6f2f26f9475cae854feeb1abf50643c5a63eee78
Rafael Martins <rafinhakafka@gmail.com>
2021-05-02 15:49:31 -0300

    update project

--- src/main/java/com/lab/person/onboarding/infrastructure/service/DownloadService.java
+++ src/main/java/com/lab/person/onboarding/infrastructure/service/DownloadService.java
@@ -1,9 +1,23 @@
 package com.lab.person.onboarding.infrastructure.service;
 
+import com.lab.person.onboarding.adapter.controller.TransactionDownloadController;
+import com.lab.person.onboarding.infrastructure.entity.TransactionEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Service
 public class DownloadService {
+
+    public void downloadCsv(HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; file=text.csv");
+        TransactionService.(response.getWriter());
+    }
+
+
 
 //    public void dowloadTransaction(HttpServletResponse response) throws IOException{
 //
